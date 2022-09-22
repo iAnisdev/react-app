a480dc44ef457506525fc9f4a5eacb17ac67f5f2
Anis <imarahmandev@gmail.com>
2022-09-22 17:43:29 +0700

    basic router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Profile from './Pages/Profile';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Setting from './Pages/Setting';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Profile">Profile</Link></li>
+        <li><Link to="/Setting">Setting</Link></li>
+        <li><Link to="/Login">Login</Link></li>
+        <li><Link to="/Signup">Signup</Link></li>
+      </ul>
+    </nav>
+    
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/Profile' element={<Profile />} />
+      <Route path='/Setting' element={<Setting />} />
+      <Route path='/Login' element={<Login />} />
+      <Route path='/Signup' element={<Signup />} />
+    </Routes>
+    </>
   );
 }
 
